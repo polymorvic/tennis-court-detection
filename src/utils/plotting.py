503e62ceed7344cb0c05254c8acca8b8f70ec17a
polymorvic <polymorphic.bite@gmail.com>
2026-05-15 21:22:57 +0200

    remove unused utility modules and classes

--- src/utils/plotting.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-from src.utils.common import ArrayLike
-
-def display_img(img: ArrayLike, title: str = "") -> None:
-    plt.imshow(img)
-    plt.title(title)
-    plt.show()
