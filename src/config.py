4638e7660715ba9491924092fb8c116729c07615
polymorvic <polymorphic.bite@gmail.com>
2026-05-16 00:16:27 +0200

    refactor: reorganize debug mode functions and ensure consistent formatting

--- src/config.py
+++ src/config.py
@@ -1,3 +1,17 @@
 import matplotlib.pyplot as plt
 
+
 plt.rcParams['image.cmap'] = 'gray'
+
+
+_DEBUG_MODE = False
+
+
+def set_debug_mode(mode: bool) -> None:
+    global _DEBUG_MODE
+    _DEBUG_MODE = mode
+
+
+def get_debug_mode() -> bool:
+    global _DEBUG_MODE
+    return _DEBUG_MODE
