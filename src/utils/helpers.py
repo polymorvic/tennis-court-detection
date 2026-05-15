94d6f4f9a57b9435901061a93b43b5178b3a690a
polymorvic <polymorphic.bite@gmail.com>
2026-05-16 01:12:31 +0200

    feat: enhance image processing functions and add validation checks

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,22 +1,96 @@
+import cv2
+import numpy as np
 from cvgeomkit.common import ArrayLike
+from cvgeomkit.geometry.lines import Line
+from cvgeomkit.utils.plotting import display_img
+
 
 from src.utils.validators import check_if_numpy_image, validate_number
+
+from src.config import get_debug_mode
+
+
+
 
 
 def crop_center_img(
         img: ArrayLike, 
         crop_ratio: float = 0.4
 ) -> tuple[ArrayLike, int, int]:
-        
         validate_number(crop_ratio, float, 0, 1, min_inclusive=False)
-        
         img = check_if_numpy_image(img)
-        
         w = img.width
-
         margin = int((1 - crop_ratio) * w / 2)
         crop = img[:, margin:w - margin]
-
         ch, cw = crop.height, crop.width
-
         return crop, ch, cw
+
+
+def net_line_scan_params(height: int) -> tuple[int, int, int]:
+    roi_h = height // 5
+    step = max(1, height // 20)
+    warmup = max(1, height // 40)
+    return roi_h, step, warmup
+
+
+def lines_from_bin_img(
+    bin_img: ArrayLike,
+    crop_img_width: int,
+    hough_thresh: int = 35,
+    min_line_len_ratio: float = 0.05,
+    min_line_gap: int = 30
+) -> list[Line] | None:
+    
+    # walidacja inputów 
+
+    edges = cv2.Canny(bin_img, 25, 100)
+    segments = cv2.HoughLinesP(
+        edges,
+        rho=1,
+        theta=np.pi / 180,
+        threshold=hough_thresh,
+        minLineLength=int(min_line_len_ratio * crop_img_width),
+        maxLineGap=min_line_gap
+    )
+
+    if segments is None:
+         return
+    
+    return [Line.from_hough_line(*segment) for segment in segments]
+
+
+def get_horizontal_lines(
+    lines: list[Line],
+    slope_thresh: float = 0.1
+) -> list[Line] | None:
+    
+    # walidacja inoputów
+    lines = [line for line in lines if line.slope is not None and abs(line.slope) < slope_thresh]
+    return lines if lines else None
+
+
+def straighten_rows(
+    bin_img : ArrayLike,
+    white_ratio_threshold: float = 0.45
+) -> ArrayLike:
+    
+    # walidacja inoputów
+    bin_img = check_if_numpy_image(bin_img)
+    bin_img_out = bin_img.copy()
+
+    w = bin_img.width
+
+    white_counts = np.count_nonzero(bin_img_out > 0, axis=1)
+    threshold = white_ratio_threshold * w
+
+    rows_to_fill = white_counts > threshold
+    bin_img_out[rows_to_fill, :] = 255
+
+    white_counts = np.count_nonzero(bin_img_out > 0, axis=1)
+
+    print('thresh', threshold)
+    print(white_counts)
+
+    return bin_img_out
+
+
