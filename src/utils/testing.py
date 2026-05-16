cf34a61b8015dea94bded9620e305e65841064f1
polymorvic <polymorphic.bite@gmail.com>
2026-05-17 01:02:51 +0200

    refactor: enhance testing utilities with new report generation and image drawing functions

--- src/utils/testing.py
+++ src/utils/testing.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 from pathlib import Path
 
+import cv2
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from src.schemas.testing import TestType
+from src.utils.validators import check_if_numpy_image
 
+from cvgeomkit.common import ArrayLike
 from cvgeomkit.geometry.lines import Line 
 from cvgeomkit.geometry.points import Point
 
@@ -34,8 +38,129 @@ def save_test_histogram(
 
     plt.figure()
     plt.hist(data, bins=bins, edgecolor="black")
-    plt.xlabel('Error')
+    plt.xlabel('Error px')
     plt.ylabel("Frequency")
-    plt.title(f'Histogram of {title}')
+    plt.title(f"Histogram of {title}'s error")
     plt.savefig(output_path)
     plt.close()
+
+
+def prepare_test_results_report(
+    output_path: str | Path,
+    results_data: list[dict],
+    not_found_list: list[str],
+    no_annotation_list: list[str],
+    filename: str,
+    results_sheet_name: str = 'results', 
+    stats_sheet_name: str = 'stats',
+    not_found_sheet_name: str = 'not_found',
+    no_annotation_sheet_name: str = 'no_annotation',
+) -> pd.Series | None:
+    
+    if not results_data:
+        raise ValueError("results_data is empty!")
+
+    with pd.ExcelWriter(Path(output_path) / f'{filename}.xlsx', engine='openpyxl') as writer:
+
+        results_df = pd.DataFrame(results_data)
+        error = results_df['gt_intercept'] - results_df['pred_intercept']
+        results_df['error'] = error
+        results_df['abs_error'] = error.abs()
+        results_df.to_excel(writer, sheet_name=results_sheet_name, index=False)
+
+        stats_df = (
+            results_df['error']
+            .agg(['median', 'mean', 'std', 'min', 'max'])
+            .to_frame()
+            .T
+        )
+        stats_df.to_excel(writer, sheet_name=stats_sheet_name, index=False)
+
+        not_found_df = pd.DataFrame(not_found_list, columns = ['pic_name'])
+        not_found_df.to_excel(writer, sheet_name=not_found_sheet_name, index=False)
+
+        no_annotation_df = pd.DataFrame(no_annotation_list, columns = ['pic_name'])
+        no_annotation_df.to_excel(writer, sheet_name=no_annotation_sheet_name, index=False)
+
+    return results_df['error']
+
+
+def draw_results_on_img(
+    output_path: str | Path,
+    img: ArrayLike,
+    pred_line: Line,
+    gt_line: Line,
+    filename: str
+) -> None:
+    img = check_if_numpy_image(img)
+    img_copy = img.copy()
+    p1_pred, p2_pred = pred_line.limit_to_img(img_copy)
+    p1_gt, p2_gt = gt_line.limit_to_img(img_copy)
+
+    pred_color = (255, 0, 0)   
+    gt_color = (0, 255, 0) 
+
+    cv2.line(img_copy, p1_pred, p2_pred, pred_color, 2)
+    cv2.line(img_copy, p1_gt, p2_gt, gt_color, 2)
+
+    w = img_copy.width
+
+    legend_x = w - 220
+    legend_y = 20
+
+    cv2.rectangle(
+        img_copy,
+        (legend_x - 10, legend_y - 10),
+        (w - 10, legend_y + 70),
+        (255, 255, 255),
+        -1
+    )
+
+    cv2.rectangle(
+        img_copy,
+        (legend_x - 10, legend_y - 10),
+        (w - 10, legend_y + 70),
+        (0, 0, 0),
+        1
+    )
+
+    cv2.line(
+        img_copy,
+        (legend_x, legend_y + 15),
+        (legend_x + 40, legend_y + 15),
+        pred_color,
+        3
+    )
+
+    cv2.putText(
+        img_copy,
+        "Prediction",
+        (legend_x + 50, legend_y + 20),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.6,
+        (0, 0, 0),
+        2,
+        cv2.LINE_AA
+    )
+
+    cv2.line(
+        img_copy,
+        (legend_x, legend_y + 45),
+        (legend_x + 40, legend_y + 45),
+        gt_color,
+        3
+    )
+
+    cv2.putText(
+        img_copy,
+        "Ground Truth",
+        (legend_x + 50, legend_y + 50),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.6,
+        (0, 0, 0),
+        2,
+        cv2.LINE_AA
+    )
+
+    cv2.imwrite(str(Path(output_path) / filename), cv2.cvtColor(img_copy, cv2.COLOR_RGB2BGR))
+
---
+++ tests/lines.py
@@ -0,0 +1,98 @@
+import tyro
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import matplotlib.pyplot as plt
+from src.utils.annotations import TennisCourtAnnotationCollection
+from src.utils.testing import (build_output_dir, gt_hline_from_annotation, 
+                               save_test_histogram, prepare_test_results_report, draw_results_on_img)
+from src.utils.helpers import load_process_params, load_pics_blacklist
+from src.schemas.testing import TestType
+from src.court_detector import CourtDetector
+from src.utils.annotations import transform_keypoint_annotation
+
+from cvgeomkit.utils.helpers import read_image_as_numpyimage
+from cvgeomkit.geometry.points import Point
+
+def run(
+    test_type: TestType,
+    pics_path: Path | str = 'data/pics',
+    params_path: Path | str = 'config/process_params.config.json',
+    blacklist_path: Path | str = 'config/pics_blacklist.config.yaml',
+    annotation_path: Path | str = 'data/annotations.json',
+    output_dir: Path | str = 'results/lines'
+):
+    '''
+    uv run python -m tests.lines --test-type SERVICE_LINE
+    '''
+    proj_cwd = Path.cwd()
+    pics_path = proj_cwd / pics_path
+    test_out_dir = build_output_dir(proj_cwd / output_dir, test_type)
+
+    tcac = TennisCourtAnnotationCollection.from_clean_file(annotation_path)
+    params = load_process_params(params_path)
+    blacklist = load_pics_blacklist(blacklist_path).blacklist
+
+    results = []
+    not_found = []
+    no_annotation = []
+    for file in tqdm(sorted(pics_path.glob("*png"))):
+
+        if file.name in blacklist:
+            continue
+
+        ann = tcac.filter_by_image(file.name)
+        if ann is None:
+            print(f'Brak annotacji dla zdjęcia: {file.stem}')
+            no_annotation.append(file.name)
+            continue
+
+        img = read_image_as_numpyimage(file)
+
+        detector = CourtDetector(img)
+        centre_intresection = detector.scan_for_service_lines(params.match_params.service_side)
+
+        if centre_intresection is None:
+            not_found.append(file.name)
+            continue
+
+        pred_line, *_ = centre_intresection
+
+        left_service_point = Point.from_iterable(transform_keypoint_annotation(img, tcac.get_key_point_for_image(file.name, "left_service_point").coordinates.as_array))
+        right_service_point = Point.from_iterable(transform_keypoint_annotation(img, tcac.get_key_point_for_image(file.name, "right_service_point").coordinates.as_array))
+
+        gt_line = gt_hline_from_annotation(left_service_point, right_service_point)
+
+        draw_results_on_img(test_out_dir, img, pred_line, gt_line, f'{file.stem}-result.png')
+
+        results.append({
+            'pic_name': file.name,
+            'pred_line': pred_line,
+            'gt_line': gt_line,
+            'pred_intercept': pred_line.intercept,
+            'gt_intercept': gt_line.intercept,
+        })
+
+    intercept_errors = prepare_test_results_report(
+        test_out_dir,
+        results,
+        not_found,
+        no_annotation,
+        f'{test_type.value}-report'
+
+    )
+
+    save_test_histogram(
+        test_out_dir, 
+        intercept_errors, 
+        f'{test_type.value}-hist', 
+        test_type.value
+    )
+
+    
+
+
+
+if __name__ == '__main__':
+    tyro.cli(run)
