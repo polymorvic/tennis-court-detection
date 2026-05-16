72deff435fca99c33b0b05ff61738842b980dd27
polymorvic <polymorphic.bite@gmail.com>
2026-05-16 23:17:13 +0200

    refactor: reorganize imports and add gt_hline_from_annotation function for line generation

--- src/utils/testing.py
+++ src/utils/testing.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 from pathlib import Path
 
+import numpy as np
+
 from src.schemas.testing import TestType
+
+from cvgeomkit.geometry.lines import Line 
+from cvgeomkit.geometry.points import Point
 
 
 def build_output_dir(parent_dir: str | Path, test_type: TestType) -> Path:
@@ -9,3 +14,8 @@ def build_output_dir(parent_dir: str | Path, test_type: TestType) -> Path:
     out_dir = Path(parent_dir) / test_type.value / ts
     out_dir.mkdir(parents=True, exist_ok=True)
     return out_dir
+
+
+def gt_hline_from_annotation(p1: Point, p2: Point,) -> Line:
+    approx_intercept = np.median(np.array([p1.y, p2.y])).astype(np.int64)
+    return Line(slope=0, intercept=approx_intercept)
