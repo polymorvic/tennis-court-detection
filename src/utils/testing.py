77c0c36b417df8dbefba7de111fd3f5fd519d275
polymorvic <polymorphic.bite@gmail.com>
2026-05-16 23:40:13 +0200

    refactor: add save_test_histogram function for histogram visualization

--- src/utils/testing.py
+++ src/utils/testing.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from pathlib import Path
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from src.schemas.testing import TestType
 
@@ -19,3 +20,22 @@ def build_output_dir(parent_dir: str | Path, test_type: TestType) -> Path:
 def gt_hline_from_annotation(p1: Point, p2: Point,) -> Line:
     approx_intercept = np.median(np.array([p1.y, p2.y])).astype(np.int64)
     return Line(slope=0, intercept=approx_intercept)
+
+
+def save_test_histogram(
+    output_path: Path | str,
+    data: np.ndarray,
+    filename: str,
+    title: str
+) -> None:
+    is_int_like = np.allclose(data, np.round(data))
+    bins = np.arange(int(data.min()), int(data.max()) + 2, 10) if is_int_like else "auto"
+    output_path = Path(output_path) / f"{filename}.png"
+
+    plt.figure()
+    plt.hist(data, bins=bins, edgecolor="black")
+    plt.xlabel('Error')
+    plt.ylabel("Frequency")
+    plt.title(f'Histogram of {title}')
+    plt.savefig(output_path)
+    plt.close()
