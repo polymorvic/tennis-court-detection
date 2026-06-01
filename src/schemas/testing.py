b942adea268bbc0dabdb3ec88e576bbec21032d3
polymorvic <polymorphic.bite@gmail.com>
2026-06-01 18:55:27 +0200

    Refactor: Remove unused schemas and utility functions
    - Deleted the following files as they are no longer needed:
      - src/schemas/__init__.py
      - src/schemas/annotations.py
      - src/schemas/config.py
      - src/schemas/court.py
      - src/schemas/testing.py
      - src/tennis_court_detection/utils/dataset_preparation.py
      - src/utils/__init__.py
      - src/utils/annotations.py
      - src/utils/errors.py
      - src/utils/filters.py
      - src/utils/helpers.py
      - src/utils/images.py
      - src/utils/testing.py
      - src/utils/validators.py
    
    This cleanup helps streamline the codebase by removing obsolete components.

--- src/schemas/testing.py
+++
@@ -1,8 +0,0 @@
-from enum import StrEnum
-
-
-class TestType(StrEnum):
-    SERVICE_LINE = 'service_line'
-    BASELINE = 'baseline'
-    CENTRE_SERVICE_LINE = 'centre_service_line'
-    CENTRE_SERVICE_POINT = 'centre_service_point'
