b55f2de677c7bcbd78fdc1095238d286cd291fc3
polymorvic <polymorphic.bite@gmail.com>
2026-06-01 08:52:44 +0200

    feat: add script to prepare dataset for shot classification

---
+++ scripts/shot_classification_dataset.py
@@ -0,0 +1,14 @@
+from tennis_court_detection.training.dataset_preparation import prepare_dataset_for_shot_classification
+import tyro
+
+
+def run(
+
+):
+    print("Preparing dataset for shot classification...")
+
+
+
+
+if __name__ == "__main__":
+    tyro.cli(run)
