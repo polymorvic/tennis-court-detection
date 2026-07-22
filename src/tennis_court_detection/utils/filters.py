eb3f3bcc9037ed2cbd4f5b942908346a35f40dbe
polymorvic <polymorphic.bite@gmail.com>
2026-07-22 12:13:44 +0200

    add function check_is_service_line to ensure service line

--- src/tennis_court_detection/utils/filters.py
+++ src/tennis_court_detection/utils/filters.py
@@ -1,13 +1,15 @@
 import cv2
 from cvgeomkit.common import ArrayLike, Numeric
 from cvgeomkit.geometry.points import Point
-from cvgeomkit.geometry.intersections import Intersection
+from cvgeomkit.geometry.segments import LineSegment
+from cvgeomkit.geometry.intersections import Intersection, transform_intersection
 from tennis_court_detection.schemas.config import ServiceSide
 from cvgeomkit.geometry.lines import Line, transform_line
 from cvgeomkit.utils.plotting import display_img
 
 from tennis_court_detection.utils.validators import check_if_numpy_image, validate_number
-from tennis_court_detection.utils.helpers import lines_from_gray_img
+from tennis_court_detection.utils.helpers import lines_from_gray_img, angle_between_lines
+from tennis_court_detection.config import get_debug_mode
 
 
 def filter_horizontal_lines(
@@ -166,3 +168,76 @@ def ensure_is_baseline(
     return baseline_candidate, count > candidates_count, lines
 
 
+def check_is_service_line(
+    img: ArrayLike,
+    line_segments: list[LineSegment],
+    middle_ratio: float = 0.3,
+    h_delta_up_ratio: float = 0.028,
+    h_delta_bottom_ratio: float = 0.005,
+    canny_lower_thresh: int = 20,
+    canny_upper_thresh: int = 100,
+    hough_thresh: int = 20,
+    min_line_len_ratio: float = 0.2,
+    max_line_gap_ratio: float = 0.1
+) -> list[Intersection] | None:
+    '''
+    sprawdza czy line segment reprezentuje service line
+    bierze srodek listy line segmentów (bo tam w tej okolicy jest najpewniej pionowa protopadla linia)
+    bierzemy wyciniki po tych segmentach, sprawdzamy czy jest tam prostopadla linia
+    jesli tak to bierzemy po obu stronach pionowe linie i szukamy intersekcji z dolna linia horyzontalna
+    jesli nic nie znaleziono to oznacza ze to nie jest service line
+    '''
+    if not 0.0 <= middle_ratio <= 1.0:
+        raise ValueError("middle_ratio must be between 0.0 and 1.0")
+    
+    img = check_if_numpy_image(img)
+
+    keep = round(len(line_segments) * middle_ratio)
+    start = (len(line_segments) - keep) // 2
+
+    h_delta_up_px = int(h_delta_up_ratio * img.height)
+    h_delta_bottom_px = int(h_delta_bottom_ratio * img.height)
+    ls_to_check = line_segments[start:start + keep]
+
+    for ls in ls_to_check:
+        roi = img[ls.start.y - h_delta_up_px: ls.start.y + h_delta_bottom_px, ls.start.x:ls.end.x]
+        min_line_len_px = int(min_line_len_ratio * roi.height)
+        max_line_gap_px = int(max_line_gap_ratio * roi.height)
+        roi_gray = cv2.cvtColor(roi, cv2.COLOR_RGB2GRAY)
+
+        lines = lines_from_gray_img(
+            roi_gray, 
+            canny_lower_thresh, 
+            canny_upper_thresh, 
+            hough_thresh, 
+            min_line_len_px, 
+            max_line_gap_px
+        )
+
+        if get_debug_mode():
+            roi_copy = roi.copy()
+            for line in lines:
+                p1, p2 = line.limit_to_img(roi_copy)
+                cv2.line(roi_copy, p1, p2, (255, 0, 0), 1)
+            display_img(roi_copy)
+
+        if not lines:
+            continue
+
+        h_line = sorted(filter_horizontal_lines(lines), key = lambda line: line.intercept, reverse=True)[0]
+        v_lines = filter_horizontal_lines(lines, horizontal=False, include_none_slope=True)
+
+        if not v_lines:
+            continue
+
+        intersections = []
+        for v_line in v_lines:
+            angle = angle_between_lines(h_line, v_line)
+
+            if 85 <= angle <= 95 or 265 <= angle <= 275:
+                inter = v_line.intersection(h_line, roi)
+                global_inter = transform_intersection(inter, roi, ls.start.x, ls.start.y - h_delta_up_px)
+                intersections.append(global_inter)
+
+        return intersections
+            
