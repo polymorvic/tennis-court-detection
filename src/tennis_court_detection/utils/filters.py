aadc84001c96cd90a5e1f3b7c460b229250f4db2
polymorvic <polymorphic.bite@gmail.com>
2026-08-13 07:55:52 +0200

    add function filter_horizontal_lines_by_white_pixels to get lines without noise

--- src/tennis_court_detection/utils/filters.py
+++ src/tennis_court_detection/utils/filters.py
@@ -1,5 +1,6 @@
 import cv2
-from cvgeomkit.common import ArrayLike, Numeric
+import numpy as np
+from cvgeomkit.common import ArrayLike, Numeric, NumpyImage
 from cvgeomkit.geometry.points import Point
 from cvgeomkit.geometry.segments import LineSegment
 from cvgeomkit.geometry.intersections import Intersection, transform_intersection
@@ -8,7 +9,12 @@ from cvgeomkit.geometry.lines import Line, transform_line
 from cvgeomkit.utils.plotting import display_img
 
 from tennis_court_detection.utils.validators import check_if_numpy_image, validate_number
-from tennis_court_detection.utils.helpers import lines_from_gray_img, angle_between_lines, make_odd_kernel_size
+from tennis_court_detection.utils.helpers import (
+    lines_from_gray_img, 
+    angle_between_lines, 
+    make_odd_kernel_size,
+    mask_line_neighborhood_on_edges
+)
 from tennis_court_detection.config import get_debug_mode
 
 
@@ -250,3 +256,47 @@ def check_is_service_line(
 
         return intersections
             
+
+def filter_horizontal_lines_by_white_pixels(
+    roi: ArrayLike,
+    edges: ArrayLike,
+    initial_h_lines: list[Line],
+    h_margin_img_ratio: float = 0.05,
+    w_margin_img_ratio: float = 0.1,
+    white_pixel_ratio_thresh: float = 0.07
+) -> list[Line]:
+    roi = check_if_numpy_image(roi)
+    h_margin_px = int(h_margin_img_ratio * roi.height)
+    w_margin_px = int(w_margin_img_ratio * roi.width)
+
+    white_ratios = []
+    h_lines = []
+    for line in initial_h_lines:
+        narrow_roi = NumpyImage(roi[:, w_margin_px:-w_margin_px])
+        edges_copy = edges.copy()
+        narrow_edges = edges_copy[:, w_margin_px:-w_margin_px]
+        
+        if get_debug_mode():
+            narrow_edges_copy = narrow_edges.copy()
+
+        x_coords = np.arange(0, narrow_roi.width)
+        y_coords = np.arange(0, narrow_roi.height)
+
+        y_values = line.slope * x_coords + line.intercept
+        mask = np.abs(y_values - y_coords[:, np.newaxis]) <= h_margin_px
+
+        narrow_edges[~mask] = 0
+        white_pixels_ratio = np.count_nonzero(narrow_edges) / (narrow_edges.width * h_margin_px * 2)
+        white_ratios.append(white_pixels_ratio)
+
+        if get_debug_mode():
+            mask_img = mask_line_neighborhood_on_edges(narrow_roi, narrow_edges_copy, mask)
+            display_img(mask_img)
+            print(f"white_pixels_ratio: {white_pixels_ratio}")
+
+
+        if white_pixels_ratio > white_pixel_ratio_thresh:
+            h_lines.append(line)
+
+    return h_lines
+
