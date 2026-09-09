570e4f057f23f29ed8f8941616bfd5aa112c4ba9
polymorvic <polymorphic.bite@gmail.com>
2026-09-09 23:46:58 +0200

    add functionality calling to find and visualize opposite centre service lines

--- tests/key_points.py
+++ tests/key_points.py
@@ -177,12 +177,29 @@ def run(
             **projected_points
         )
 
+        service_lines_opposite_results = detector.find_centre_service_lines_opposite(
+            netline_bottom_segments, 
+            left_centre_service_line_segments,
+            right_centre_service_line_segments,
+            service_line_opposite_segments,
+            left_inner_segments,
+            right_inner_segments,
+            centre_service_half_lines
+        )
+
+        if service_lines_opposite_results is None:
+            print(f'Brak przeciwnych linii serwisowych dla zdjęcia: {file.stem}')
+            left_centre_service_line_segments_opposite, right_centre_service_line_segments_opposite = [], []
+        else:
+            left_centre_service_line_segments_opposite, right_centre_service_line_segments_opposite = service_lines_opposite_results
+
         img_copy = img.copy()
         for segments in [baseline_segments, left_outer_segments, 
                         left_inner_segments, right_inner_segments, right_outer_segments,
                         service_line_segments, left_centre_service_line_segments, 
                         right_centre_service_line_segments, netline_bottom_segments, netline_top_segments, 
-                        baseline_opposite_segments, service_line_opposite_segments]:
+                        baseline_opposite_segments, service_line_opposite_segments,
+                        left_centre_service_line_segments_opposite, right_centre_service_line_segments_opposite ]:
             for segment in segments:
                 cv2.line(img_copy, segment.start, segment.end, (255, 0, 0), 1)
 
